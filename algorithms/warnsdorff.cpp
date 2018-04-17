d9868a1dfcce09a48de8c32f4b968a146e0cbe27
Wisnu Adi Nurcahyo <wisn.adn@gmail.com>
2018-04-18 02:09:51 +0700

    Initial commit. Backup.

---
+++ algorithms/warnsdorff.cpp
@@ -0,0 +1,226 @@
+/*!
+ * Knight's Tour
+ * Warnsdorff's rule implementation
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct neighbor {
+  // c: count, i: moves index
+  int c, i;
+
+  bool operator > (neighbor const& n) const {
+    return c > n.c;
+  }
+};
+
+int main() {
+  // Chessboard NxN
+  int n;
+  scanf("%d", &n);
+
+  // Chessboard first initialization
+  vector<vector<int>> board (n, vector<int>(n, 0));
+
+  // Knight's legal moves
+  vector<vector<int>> moves = {
+    {1, 2}, {2, 1}, {-1, 2}, {1, -2}, {-2, 1}, {2, -1}, {-2, -1}, {-1, -2}
+  };
+
+  // Knight's move checker
+  function<bool(int, int)> moveable = [n, &board](int x, int y) {
+    return x > -1 && y > -1 && x < n && y < n;
+  };
+
+  // Chessboard visits checker
+  function<bool(int, int)> visited = [&board](int x, int y) {
+    return board[x][y] > 0;
+  };
+
+  // Warnsdorff's rule implementation with Backtracking method
+  function<bool(int, int, int)> tourable =
+  [
+    moves,
+    n,
+    &board,
+    &moveable,
+    &tourable,
+    &visited
+  ] (int x, int y, int p) {
+    // Mark the chessboard
+    board[x][y] = p;
+
+    // Found a solution
+    if (p == n * n) return true;
+
+    // Min Heap for the available neighbors
+    priority_queue<neighbor, vector<neighbor>, greater<neighbor>> heap;
+
+    // Check all possible moves
+    for (int i = 0; i < moves.size(); i++) {
+      int nx = x + moves[i][0];
+      int ny = y + moves[i][1];
+
+      if (moveable(nx, ny) && !visited(nx, ny)) {
+        int count = 0;
+
+        // Check all possible neighbors
+        for (int j = 0; j < moves.size(); j++) {
+          int mx = nx + moves[j][0];
+          int my = ny + moves[j][1];
+
+          if (moveable(mx, my) && !visited(mx, my))
+            count++;
+        }
+
+        neighbor ne;
+        ne.c = count;
+        ne.i = i;
+
+        heap.push(ne);
+      }
+    }
+
+    if (heap.size() > 0) {
+      neighbor ne = heap.top();
+      int nx = moves[ne.i][0];
+      int ny = moves[ne.i][1];
+
+      if(tourable(x + nx, y + ny, p + 1))
+        return true;
+    }
+
+    // No solution
+    return false;
+  };
+
+  // Chessboard printer
+  function<void()> printBoard = [n, &board]() {
+    int maxdigit = log10(n * n);
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        int step = board[i][j];
+        int digit = log10(step);
+
+        for (int k = 0; k < maxdigit - digit; k++)
+          printf(" ");
+
+        printf(" %d", step);
+
+        if (j < n - 1) printf(" ");
+      }
+
+      printf("\n");
+    }
+  };
+
+  // Chessboard initialization helper
+  function<void()> initializeBoard = [n, &board]() {
+    vector<vector<int>> v (n, vector<int>(n, 0));
+
+    board = v;
+  };
+
+  // Find all solutions from each legal starting point
+  function<void()> findAllSolutions =
+  [
+    n,
+    &initializeBoard,
+    &printBoard,
+    &tourable
+  ]() {
+    // Used for stats
+    int fails = 0;
+    int success = 0;
+
+    clock_t start = clock();
+
+    printf(" There are %d starting points.\n", n * n);
+    printf(" Finding the solution from each starting point...\n");
+    printf("\n");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        printf(" (%d, %d)\n", i, j);
+
+        if (tourable(i, j, 1)) {
+          success++;
+          printBoard();
+        }
+        else {
+          fails++;
+          printf(" Knight's tour solution doesn't exist.\n");
+        }
+
+        printf("\n");
+
+        initializeBoard();
+      }
+    }
+
+    printf(" Took about %.2f seconds.\n", (clock() - start) / 1000000.0);
+    printf(" Success: %d starting point(s).\n", success);
+    printf(" Failure: %d starting point(s).\n", fails);
+  };
+
+  // Find the solution from a random starting point
+  function<void()> findRandomSolution =
+  [
+    &n,
+    &initializeBoard,
+    &printBoard,
+    &tourable
+  ]() {
+    srand(time(0));
+
+    int x = rand() % n;
+    int y = rand() % n;
+    int tries = 0;
+
+    clock_t start = clock();
+
+    while (!tourable(x, y, 1) && tries < n * n) {
+      tries++;
+
+      x = rand() % n;
+      y = rand() % n;
+
+      initializeBoard();
+    }
+
+    printf(" Finding a random solution from the %dx%d chessboard.\n", n, n);
+    printf("\n");
+
+    if (tries >= n * n)
+      printf(" The solution doesn't exist.\n");
+    else {
+      printf(" Found a solution at starting point");
+      printf(" (%d, %d).\n", x, y);
+      printBoard();
+    }
+
+    printf("\n");
+    printf(" Took about %.2f seconds", (clock() - start) / 1000000.0);
+    printf(" after %d", ++tries);
+
+    if (tries < 2)
+      printf(" try");
+    else
+      printf(" tries");
+
+    printf(".\n");
+  };
+
+
+  /*!
+   * Choose one from findAllSolutions or findRandomSolution helper.
+   */
+
+  findAllSolutions();
+
+  // findRandomSolution();
+
+  return 0;
+}
